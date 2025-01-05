e7f555ef35f1847b249d7a129aee99846048e433
Douglas Souza <devdouglassouza2002@hotmail.com>
2025-01-04 21:05:44 -0300

    chore: setup clerk and integrating with convex

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
 import './index.css'
 
 import { ConvexProvider, ConvexReactClient } from 'convex/react'
-import { ThemeProvider } from './components/theme-provider'
+import { ClerkProvider } from '@clerk/clerk-react'
 import { RouterProvider } from 'react-router-dom'
+
+import { ThemeProvider } from './components/theme-provider'
 import { router } from './routes'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
 
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string)
 
 export function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <ConvexProvider client={convex}>
-        <RouterProvider router={router} />
-      </ConvexProvider>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+        <ConvexProvider client={convex}>
+          <RouterProvider router={router} />
+        </ConvexProvider>
+      </ClerkProvider>
     </ThemeProvider>
   )
 }
